53b2d47a67512095e48e66c20f4688d24595a88b
Yeshimbetov Adilbek <42997517+adiida@users.noreply.github.com>
2022-08-30 13:46:49 +0600

    just temp file

---
+++ temp.py
@@ -0,0 +1,5 @@
+# prompt = 'Worgen portrait fantasy art d&d dota2 WoW Video game icon, 2d game art, official fanart behance hd artstation by Jesper Ejsing, by RHADS, Makoto Shinkai and Lois van baarle, ilya kuvshinov, rossdraws'
+
+# Paladin, Rogue, Assassin, Warrior, Fighter, Ranger, Wizard, Cleric, Monk, Bard, Druid, Rogue, Barbarian, Shaman, Priest, Hunter, Warlock, Mage, Death Knight
+# Dwarf, Elf (dark, blood, night), Orc, Goblin, Tauren, Troll, Draenei, Worgen, Pandaren, Undead, Human, Halfling, Gnome
+# Aberration, Beast, Critter, Demon, Dragonkin, Elemental, Giant, Humanoid, Mechanical, Ooze, Swarm, Monstrocity, Kobold, Lizardfolk, Merfolk, Sahuagin, Grimlock, Gnoll, Fiend (demon, devil), Fey, Celestial, Beast, Construct, Plant
